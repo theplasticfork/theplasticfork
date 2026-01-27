edfcbbc9cef02076da84b7d4fe121b1398bd6111
Pranish Lal <pranish@Pranishs-iMac.lan>
2026-01-27 14:37:11 -0800

    added privacy policy page

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,8 @@
+export default function Privacy() {
+    return (
+      <div style={{ padding: '50px', fontFamily: 'sans-serif' }}>
+        <h1>Privacy Policy</h1>
+        <p>The Plastic Fork does not sell your data. We only use your email to manage your meal plan audits.</p>
+      </div>
+    )
+  }
